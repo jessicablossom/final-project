f653fff06976e77d6470fb5ef0834a1870075088
jessica_b <jessica.francavilla86@gmail.com>
2021-12-07 12:25:27 -0300

    add responsive breakpoints , add router views

---
+++ src/views/Register.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="register">
+    <Navbar />
+    <Login />
+  </div>
+</template>
+<script>
+import Navbar from "../components/Navbar.vue";
+import Login from "../components/Login.vue";
+
+export default {
+  name: "Register",
+  components: {
+    Navbar,
+    Login,
+  },
+};
+</script>
+<style scoped>
+.register {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
